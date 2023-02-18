798596d754bd25052ef5203ba55263f86b3959a5
Eric Wong <ericwong@zju.edu.cn>
2023-02-18 15:34:24 +0800

    rename: struct names

--- types.h
+++ types.h
@@ -3,41 +3,48 @@
 
 #include <stdint.h>
 
-typedef struct {
+typedef struct _fe fe_t;
+typedef struct _ge_p1 ge_p1_t;
+typedef struct _ge_p2 ge_p2_t;
+typedef struct _ge_p3 ge_p3_t;
+typedef struct _ge_pc ge_pc_t;
+typedef struct _ge_pz ge_pz_t;
+
+struct _fe {
   int32_t d[10];
-} fe_t;
+};
 
-typedef struct {
+struct _ge_p1 {
   fe_t x;
   fe_t y;
   fe_t z;
   fe_t t;
-} ge_p1_t;
+};
 
-typedef struct {
+struct _ge_p2 {
   fe_t x;
   fe_t y;
   fe_t z;
-} ge_p2_t;
+};
 
-typedef struct {
+struct _ge_p3 {
   fe_t x;
   fe_t y;
   fe_t z;
   fe_t t;
-} ge_p3_t;
+};
 
-typedef struct {
+struct _ge_pc {
   fe_t yplusx;
   fe_t yminusx;
   fe_t xyd2;
-} ge_pc_t;
+};
 
-typedef struct {
+struct _ge_pz {
   fe_t yplusx;
   fe_t yminusx;
   fe_t td2;
   fe_t z;
-} ge_pz_t;
+};
 
 #endif  /* CURVE25519_TYPES_H */
